4d7f6ae606ab3bf5a511e75dc32a782e42ddc43c
JzwOnly <836579250@qq.com>
2022-07-14 15:23:09 +0800

    Update VideoEncoder.m

--- H264-RTSP-Server-iOS/H264-RTSP-Server-iOS/Classes/AVEncoder/VideoEncoder.m
+++ H264-RTSP-Server-iOS/H264-RTSP-Server-iOS/Classes/AVEncoder/VideoEncoder.m
@@ -12,15 +12,15 @@
 
 @synthesize path = _path;
 
-+ (VideoEncoder*) encoderForPath:(NSString*) path Height:(int) height andWidth:(int) width
++ (VideoEncoder*) encoderForPath:(NSString*) path Height:(int) height andWidth:(int) width videoTrack:(CMFormatDescription)videoTrack audioTrack:(CMFormatDescription)audioTrack
 {
     VideoEncoder* enc = [VideoEncoder alloc];
-    [enc initPath:path Height:height andWidth:width];
+    [enc initPath:path Height:height andWidth:width videoTrack:videoTrack audioTrack:audioTrack];
     return enc;
 }
 
 
-- (void) initPath:(NSString*)path Height:(int) height andWidth:(int) width
+- (void) initPath:(NSString*)path Height:(int) height andWidth:(int) width videoTrack:(CMFormatDescription)videoTrack audioTrack:(CMFormatDescription)audioTrack
 {
     self.path = path;
     
@@ -28,17 +28,50 @@
     NSURL* url = [NSURL fileURLWithPath:self.path];
     
     _writer = [AVAssetWriter assetWriterWithURL:url fileType:AVFileTypeQuickTimeMovie error:nil];
-    NSDictionary* settings = [NSDictionary dictionaryWithObjectsAndKeys:
+    
+    int videoWidth;
+    int videoHeight;
+    if (videoTrack) {
+        CMVideoDimensions dimensions = CMVideoFormatDescriptionGetDimensions(videoTrack);
+        videoWidth = dimensions.width;
+        videoHeight = dimensions.height;
+    } else {
+        videoWidth = width
+        videoHeight = height;
+    }
+    
+    // video inputs
+    NSDictionary* videoSettings = [NSDictionary dictionaryWithObjectsAndKeys:
                               AVVideoCodecH264, AVVideoCodecKey,
-                              [NSNumber numberWithInt: width], AVVideoWidthKey,
-                              [NSNumber numberWithInt:height], AVVideoHeightKey,
+                              [NSNumber numberWithInt:videoWidth], AVVideoWidthKey,
+                              [NSNumber numberWithInt:videoHeight], AVVideoHeightKey,
                               [NSDictionary dictionaryWithObjectsAndKeys:
                                     @YES, AVVideoAllowFrameReorderingKey, nil],
                                     AVVideoCompressionPropertiesKey,
                               nil];
-    _writerInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:settings];
-    _writerInput.expectsMediaDataInRealTime = YES;
-    [_writer addInput:_writerInput];
+    if (videoTrack) {
+        _videoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:videoSettings sourceFormatHint:videoTrack];
+    } else {
+        _videoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:videoSettings];
+    }
+    _videoInput.expectsMediaDataInRealTime = YES;
+    if (_writer canAddInput:_videoInput) {
+        [_writer addInput:_videoInput];
+    }
+    
+    // audio inputs
+    NSDictionary* audioSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+                              [NSNumber numberWithInt:kAudioFormatMPEG4AAC], AVFormatIDKey,
+                              nil];
+    if (audioTrack) {
+        _audioInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio outputSettings:audioSettings sourceFormatHint:audioTrack];
+    } else {
+        _audioInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio outputSettings:audioSettings];
+    }
+    _audioInput.expectsMediaDataInRealTime = YES;
+    if (_writer canAddInput:_audioInput) {
+        [_writer addInput:_audioInput];
+    }
 }
 
 - (void) finishWithCompletionHandler:(void (^)(void))handler
@@ -46,7 +79,7 @@
     [_writer finishWritingWithCompletionHandler: handler];
 }
 
-- (BOOL) encodeFrame:(CMSampleBufferRef) sampleBuffer
+- (BOOL) encodeFrame:(CMSampleBufferRef) sampleBuffer mediaType:(AVMediaType)mediaType
 {
     if (CMSampleBufferDataIsReady(sampleBuffer))
     {
@@ -61,9 +94,10 @@
             NSLog(@"writer error %@", _writer.error.localizedDescription);
             return NO;
         }
-        if (_writerInput.readyForMoreMediaData == YES)
+        AVAssetWriterInput * writerInput = (mediaType == AVMediaTypeVideo ? _videoInput : _audioInput;
+        if (writerInput.readyForMoreMediaData == YES)
         {
-            [_writerInput appendSampleBuffer:sampleBuffer];
+            [writerInput appendSampleBuffer:sampleBuffer];
             return YES;
         }
     }
